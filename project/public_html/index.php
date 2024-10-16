d0004d1c71db1436e59e560ed0511a50468772dd
Misha <mishaho@mail.com>
2024-10-16 11:43:34 +0200

    navbar

--- project/public_html/index.php
+++ project/public_html/index.php
@@ -28,6 +28,31 @@ if (!isset($_SESSION['userid'])) {
 </head>
 <body class="bg-dark text-light">
     <div class="container text-center mt-5">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+        <br>
         <h1>Dashboard</h1>
         <p><?php echo $message; ?></p>
     </div>
