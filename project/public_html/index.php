dfffc0314fa919cfdbb4beadb0b6939d1b0ef901
Diego <113429105+trueeman@users.noreply.github.com>
2024-10-18 09:08:18 +0200

    Revert "18 tonen van geregistreerde verkiezingen met bijbehorende partijen"

--- project/public_html/index.php
+++ project/public_html/index.php
@@ -26,9 +26,6 @@ if (!isset($_SESSION['userid'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css/theme.css">
 </head>
-</div>
-
-
 <body class="bg-dark text-light">
     <div class="container text-center mt-5">
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
@@ -48,21 +45,13 @@ if (!isset($_SESSION['userid'])) {
                             <a class="nav-link disabled" aria-disabled="true">Stemmen</a>
                         </li>
                     </ul>
-                    <<ul class="navbar-nav ms-auto d-flex">
-                 <?php if (isset($_SESSION["userid"])) : ?>
-                    <li class="nav-item me-2">
-                    <a class="nav-link" href="logout.php">Uitloggen</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="register_partij.php">Partij registreren</a>
-                    </li>
-                 <?php else : ?>
-                    <li class="nav-item">
-                    <a class="nav-link" href="login.php">Inloggen</a>
-                    </li>
-                 <?php endif; ?>
-                    </ul>
-
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <?php if (isset($_SESSION["userid"])) : ?>
+                                <a class="nav-link" href="logout.php">Uitloggen</a>
+                            <?php else : ?>
+                                <a class="nav-link" href="login.php">Inloggen</a>
+                            <?php endif; ?>
                         </li>
                     </ul>
                 </div>
